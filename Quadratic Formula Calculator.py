28860fd7a95233fb32723056860d4f694799e3d3
walkerjb02 <88942620+walkerjb02@users.noreply.github.com>
2021-08-20 00:41:38 -0400

    Update Quadratic Formula Calculator.py

--- Quadratic Formula Calculator.py
+++ Quadratic Formula Calculator.py
@@ -3,6 +3,6 @@ from math import sqrt
 def quadratic_formula(a,b,c):
     x = (-b + sqrt((b*b)-4*a*c))/(2 * a)
     y = (-b - sqrt((b*b)-4*a*c))/(2 * a)
-    print('Your first value is: {} and your second value is: {}'.format(x,y))
+    print('Your first x-intercept is: {} and your second is: {}'.format(x,y))
 print(quadratic_formula(int(input("input an a: ")), int(input("input an b: ")), int(input("input an c: "))))
 input('Press ENTER to exit')
